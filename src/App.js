9e3f3e1f4abcd48b2ac39cd2142af9bb3e52d880
Agwenchez <agweraenock76@gmail.com>
2019-08-07 19:28:23 +0300

    some editing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-
-
+// import logo from './logo.svg';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>hello world</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">column one</div>
+          <div className="col-6">
+            <span>
+            <i className="fas fa-home"></i> 
+            </span>
+          </div>
+        </div>
       </div>
     );
   }
